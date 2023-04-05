5c1b3a86722fb005ab850cac0bbd6c4312136dd1
sultandilaram <sultanndilaram@gmail.com>
2023-04-05 14:44:34 +0500

    minor

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://profiles.honeycombprotocol.com"],
+    origin: ["https://profiles.honeycombprotocol.com", "http://localhost:3000"],
     methods: ["POST", "PUT", "GET", "DELETE"],
     credentials: true,
   })
